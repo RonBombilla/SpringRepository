96721d8cab5208c62973282e166f84b29de4105b
RonBombilla <62183965+RonBombilla@users.noreply.github.com>
2020-06-12 09:25:32 -0400

    Security Config
    se actualizo la seguridad general para acceder a la página

---
+++ WazuWeb/src/main/java/com/wazuweb/repo/IUsuarioLog.java
@@ -0,0 +1,11 @@
+package com.wazuweb.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.wazuweb.model.UsuarioSecurity;
+
+public interface IUsuarioLog extends JpaRepository <UsuarioSecurity, Integer> {
+	
+	UsuarioSecurity findByNombre (String nombre);
+
+}
--- WazuWeb/src/test/java/com/wazuweb/WazuWebApplicationTests.java
+++ WazuWeb/src/test/java/com/wazuweb/WazuWebApplicationTests.java
@@ -1,13 +1,35 @@
 package com.wazuweb;
 
+import static org.junit.Assert.assertTrue;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import com.wazuweb.model.UsuarioSecurity;
+import com.wazuweb.repo.IUsuarioLog;
+
+@RunWith (SpringRunner.class)
 @SpringBootTest
 class WazuWebApplicationTests {
 
+	@Autowired
+	private BCryptPasswordEncoder encoder;
+	
+	@Autowired
+	private IUsuarioLog log;
+	
 	@Test
-	void contextLoads() {
+	public void crearUsuarioTest() {
+		UsuarioSecurity us = new UsuarioSecurity();
+		us.setId(1);
+		us.setNombre("Aaron");
+		us.setClave(encoder.encode("kkhuate-6651"));
+		UsuarioSecurity retorno = log.save(us);
+		
+		assertTrue(retorno.getClave().equalsIgnoreCase(us.getClave()));
 	}
 
 }
